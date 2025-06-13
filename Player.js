b010696962ea9f198eae6220349b9f9694096328
dtvk027 <dtvk922@gmail.com>
2025-06-13 15:47:45 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,85 +1,135 @@
 export default class Player {
-  constructor(ctx, width, height, canvasHeight, scaleRatio) {
-    this.ctx = ctx;
-    this.width = width;
-    this.height = height;
-    this.scaleRatio = scaleRatio;
+    WALK_ANIMATION_TIMER = 100;
+    walkAnimationTimer = this.WALK_ANIMATION_TIMER;
+    dinoRunImages = [];
+    currentAnimationFrame = 0;
 
-    this.x = 10 * scaleRatio;
-    this.y = canvasHeight - height - 1.5 * scaleRatio;
-    this.yStandingPosition = this.y;
+    jumpPressed = false;
+    jumpInProgress = false;
+    falling = false;
+    JUMP_SPEED = 0.6;
+    GRAVITY = 0.4;
+    MAX_JUMP_HEIGHT = 0;
+    MIN_JUMP_HEIGHT = 0;
 
-    this.velocityY = 0;
-    this.jumpPressed = false;
-    this.jumpInProgress = false;
-    this.falling = false;
+    constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio) {
+        this.ctx = ctx;
+        this.canvas = ctx.canvas;
+        this.width = width;
+        this.height = height;
+        this.minJumpHeight = minJumpHeight;
+        this.maxJumpHeight = maxJumpHeight;
+        this.scaleRatio = scaleRatio;
 
-    // Animation
-    this.image = new Image();
-    this.image.src = 'images/dino_run1.png';
-    this.runImages = [
-      'images/dino_run1.png',
-      'images/dino_run2.png'
-    ].map(src => {
-      const img = new Image();
-      img.src = src;
-      return img;
-    });
-    this.currentRunImage = 0;
-    this.animationTimer = 0;
-    this.ANIMATION_INTERVAL = 100;
+        this.x = 10 * scaleRatio;
+        this.y = this.canvas.height - this.height - 1.5 * scaleRatio;
+        this.yStandingPosition = this.y;
 
-    this.setupControls();
-  }
+        // Load all sprite images
+        this.standingStillImage = new Image();
+        this.standingStillImage.src = "images/standing_still.png";
+        this.image = this.standingStillImage;
 
-  setupControls() {
-    window.addEventListener('keydown', (e) => {
-      if (e.code === 'Space') this.jumpPressed = true;
-    });
-    window.addEventListener('keyup', (e) => {
-      if (e.code === 'Space') this.jumpPressed = false;
-    });
-    window.addEventListener('touchstart', () => this.jumpPressed = true);
-    window.addEventListener('touchend', () => this.jumpPressed = false);
-  }
+        const dinoRunImage1 = new Image();
+        dinoRunImage1.src = "images/dino_run1.png";
 
-  update(gameSpeed, frameTimeDelta) {
-    this.handleRunAnimation(gameSpeed, frameTimeDelta);
-    this.handleJump(frameTimeDelta);
-  }
+        const dinoRunImage2 = new Image();
+        dinoRunImage2.src = "images/dino_run2.png";
 
-  handleRunAnimation(gameSpeed, frameTimeDelta) {
-    this.animationTimer += frameTimeDelta * gameSpeed;
-    if (this.animationTimer >= this.ANIMATION_INTERVAL) {
-      this.animationTimer = 0;
-      this.currentRunImage = (this.currentRunImage + 1) % this.runImages.length;
-      this.image = this.runImages[this.currentRunImage];
-    }
-  }
+        this.dinoRunImages.push(dinoRunImage1);
+        this.dinoRunImages.push(dinoRunImage2);
 
-  handleJump(frameTimeDelta) {
-    const GRAVITY = 0.4 * this.scaleRatio;
-    const JUMP_FORCE = 12 * this.scaleRatio;
-
-    if (this.jumpPressed && !this.jumpInProgress && !this.falling) {
-      this.velocityY = -JUMP_FORCE;
-      this.jumpInProgress = true;
+        // Initialize event listeners
+        this.initializeEventListeners();
     }
 
-    this.y += this.velocityY;
-    this.velocityY += GRAVITY;
+    initializeEventListeners() {
+        // Clear existing listeners to prevent duplicates
+        window.removeEventListener("keydown", this.keydown);
+        window.removeEventListener("keyup", this.keyup);
+        window.removeEventListener("touchstart", this.touchstart);
+        window.removeEventListener("touchend", this.touchend);
 
-    if (this.y >= this.yStandingPosition) {
-      this.y = this.yStandingPosition;
-      this.velocityY = 0;
-      this.jumpInProgress = false;
-      this.falling = false;
-    } else {
-      this.falling = true;
+        // Keyboard controls
+        window.addEventListener("keydown", this.keydown.bind(this));
+        window.addEventListener("keyup", this.keyup.bind(this));
+
+        // Touch controls
+        window.addEventListener("touchstart", this.touchstart.bind(this));
+        window.addEventListener("touchend", this.touchend.bind(this));
     }
-  }
 
-  draw() {
-    this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-  }
+    touchstart() {
+        this.jumpPressed = true;
+    }
+
+    touchend() {
+        this.jumpPressed = false;
+    }
+
+    keydown(event) {
+        if (event.code === "Space") {
+            this.jumpPressed = true;
+            event.preventDefault();
+        }
+    }
+
+    keyup(event) {
+        if (event.code === "Space") {
+            this.jumpPressed = false;
+        }
+    }
+
+    update(gameSpeed, frameTimeDelta) {
+        this.run(gameSpeed, frameTimeDelta);
+
+        if (this.jumpInProgress) {
+            this.image = this.standingStillImage;
+        }
+
+        this.jump(frameTimeDelta);
+    }
+
+    jump(frameTimeDelta) {
+        if (this.jumpPressed && !this.jumpInProgress && !this.falling) {
+            this.jumpInProgress = true;
+            this.falling = false;
+        }
+
+        if (this.jumpInProgress && !this.falling) {
+            if (this.y > this.canvas.height - this.minJumpHeight || 
+                (this.y > this.canvas.height - this.maxJumpHeight && this.jumpPressed)) {
+                this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio;
+            } else {
+                this.falling = true;
+            }
+        } else {
+            if (this.y < this.yStandingPosition) {
+                this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
+                if (this.y + this.height > this.canvas.height) {
+                    this.y = this.yStandingPosition;
+                }
+            } else {
+                this.falling = false;
+                this.jumpInProgress = false;
+            }
+        }
+    }
+
+    run(gameSpeed, frameTimeDelta) {
+        if (this.walkAnimationTimer <= 0 && !this.jumpInProgress) {
+            this.currentAnimationFrame = (this.currentAnimationFrame + 1) % this.dinoRunImages.length;
+            this.image = this.dinoRunImages[this.currentAnimationFrame];
+            this.walkAnimationTimer = this.WALK_ANIMATION_TIMER;
+        }
+        this.walkAnimationTimer -= frameTimeDelta * gameSpeed;
+    }
+
+    draw() {
+        this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        
+        // Debug hitbox (uncomment to see)
+        // this.ctx.strokeStyle = 'red';
+        // this.ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
 }
