1e2615e34728bb6a20ab6eaef79b4459a271e226
dtvk027 <dtvk922@gmail.com>
2025-06-13 15:39:35 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,122 +1,82 @@
 export default class Player {
-  WALK_ANIMATION_TIMER = 100;
-  walkAnimationTimer = this.WALK_ANIMATION_TIMER;
-  dinoRunImages = [];
-
-  jumpPressed = false;
-  jumpInProgress = false;
-  falling = false;
-  JUMP_SPEED = 0.6;
-  GRAVITY = 0.4;
-
-  constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio) {
+  constructor(ctx, width, height, canvasHeight, scaleRatio) {
     this.ctx = ctx;
-    this.canvas = ctx.canvas;
     this.width = width;
     this.height = height;
-    this.minJumpHeight = minJumpHeight;
-    this.maxJumpHeight = maxJumpHeight;
     this.scaleRatio = scaleRatio;
 
     this.x = 10 * scaleRatio;
-    this.y = this.canvas.height - this.height - 1.5 * scaleRatio;
+    this.y = canvasHeight - height - 1.5 * scaleRatio;
     this.yStandingPosition = this.y;
 
-    this.standingStillImage = new Image();
-    this.standingStillImage.src = "images/dino_run1.png";
-    this.image = this.standingStillImage;
+    this.velocityY = 0;
+    this.jumpPressed = false;
+    this.jumpInProgress = false;
+    this.falling = false;
 
-    const dinoRunImage1 = new Image();
-    dinoRunImage1.src = "images/dino_run1.png";
+    // Animation
+    this.image = new Image();
+    this.image.src = 'images/dino_run1.png';
+    this.runImages = [
+      'images/dino_run1.png',
+      'images/dino_run2.png'
+    ].map(src => {
+      const img = new Image();
+      img.src = src;
+      return img;
+    });
+    this.currentRunImage = 0;
+    this.animationTimer = 0;
+    this.ANIMATION_INTERVAL = 100;
 
-    const dinoRunImage2 = new Image();
-    dinoRunImage2.src = "images/dino_run2.png";
-
-    this.dinoRunImages.push(dinoRunImage1);
-    this.dinoRunImages.push(dinoRunImage2);
-
-    //keyboard
-    window.removeEventListener("keydown", this.keydown);
-    window.removeEventListener("keyup", this.keyup);
-
-    window.addEventListener("keydown", this.keydown);
-    window.addEventListener("keyup", this.keyup);
-
-    //touch
-    window.removeEventListener("touchstart", this.touchstart);
-    window.removeEventListener("touchend", this.touchend);
-
-    window.addEventListener("touchstart", this.touchstart);
-    window.addEventListener("touchend", this.touchend);
+    this.setupControls();
   }
 
-  touchstart = () => {
-    this.jumpPressed = true;
-  };
-
-  touchend = () => {
-    this.jumpPressed = false;
-  };
-
-  keydown = (event) => {
-    if (event.code === "Space") {
-      this.jumpPressed = true;
-    }
-  };
-
-  keyup = (event) => {
-    if (event.code === "Space") {
-      this.jumpPressed = false;
-    }
-  };
+  setupControls() {
+    window.addEventListener('keydown', (e) => {
+      if (e.code === 'Space') this.jumpPressed = true;
+    });
+    window.addEventListener('keyup', (e) => {
+      if (e.code === 'Space') this.jumpPressed = false;
+    });
+    window.addEventListener('touchstart', () => this.jumpPressed = true);
+    window.addEventListener('touchend', () => this.jumpPressed = false);
+  }
 
   update(gameSpeed, frameTimeDelta) {
-    this.run(gameSpeed, frameTimeDelta);
-
-    if (this.jumpInProgress) {
-      this.image = this.standingStillImage;
-    }
-
-    this.jump(frameTimeDelta);
+    this.handleRunAnimation(gameSpeed, frameTimeDelta);
+    this.handleJump(frameTimeDelta);
   }
 
-  jump(frameTimeDelta) {
-    if (this.jumpPressed) {
+  handleRunAnimation(gameSpeed, frameTimeDelta) {
+    this.animationTimer += frameTimeDelta * gameSpeed;
+    if (this.animationTimer >= this.ANIMATION_INTERVAL) {
+      this.animationTimer = 0;
+      this.currentRunImage = (this.currentRunImage + 1) % this.runImages.length;
+      this.image = this.runImages[this.currentRunImage];
+    }
+  }
+
+  handleJump(frameTimeDelta) {
+    const GRAVITY = 0.4 * this.scaleRatio;
+    const JUMP_FORCE = 12 * this.scaleRatio;
+
+    if (this.jumpPressed && !this.jumpInProgress && !this.falling) {
+      this.velocityY = -JUMP_FORCE;
       this.jumpInProgress = true;
     }
 
-    if (this.jumpInProgress && !this.falling) {
-      if (
-        this.y > this.canvas.height - this.minJumpHeight ||
-        (this.y > this.canvas.height - this.maxJumpHeight && this.jumpPressed)
-      ) {
-        this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio;
-      } else {
-        this.falling = true;
-      }
-    } else {
-      if (this.y < this.yStandingPosition) {
-        this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
-        if (this.y + this.height > this.canvas.height) {
-          this.y = this.yStandingPosition;
-        }
-      } else {
-        this.falling = false;
-        this.jumpInProgress = false;
-      }
-    }
-  }
+    this.y += this.velocityY;
+    this.velocityY += GRAVITY;
 
-  run(gameSpeed, frameTimeDelta) {
-    if (this.walkAnimationTimer <= 0) {
-      if (this.image === this.dinoRunImages[0]) {
-        this.image = this.dinoRunImages[1];
-      } else {
-        this.image = this.dinoRunImages[0];
-      }
-      this.walkAnimationTimer = this.WALK_ANIMATION_TIMER;
+    if (this.y >= this.yStandingPosition) {
+      this.y = this.yStandingPosition;
+      this.velocityY = 0;
+      this.jumpInProgress = false;
+      this.falling = false;
+    } else {
+      this.falling = true;
     }
-    this.walkAnimationTimer -= frameTimeDelta * gameSpeed;
   }
 
   draw() {
